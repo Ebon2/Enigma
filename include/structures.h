8f7c341cd4336c78d185c683ab2f21d245742224
Ebon2 <luis.mgcn@gmail.com>
2025-11-10 00:06:43 -0600

    code documented using Doxygen

--- include/structures.h
+++ include/structures.h
@@ -1,30 +1,50 @@
-//
-// Created by angelrojas on 21/10/25.
-//
+/**
+ * @file structures.h
+ * @brief This file contains the enums and data structures.
+ * @author Angel Rojas
+ * @date 2024-11-10
+ * @version 1.1
+ * @details
+ * This file serves as a repository for data structures used throughout the program.
+ * It includes definitions for barrel and reflector configurations, file paths, and
+ * other program-wide settings.
+ */
 
 #ifndef structures_H
 #define structures_H
 
+/**
+ * @brief Enumerates different levels of encryption or decryption.
+ */
 typedef enum {
-    ENCRYPT,
-    DECRYPT,
+    ENCRYPT, /**< Encryption mode. */
+    DECRYPT, /**< Decryption mode. */
 } mod_t;
 
+/**
+ * @brief Enumerates different levels of encryption or decryption.
+ */
 typedef enum {
-    NONE = -1,
-    LOW = 1,
-    HIGH = 2,
-    EXTREME = 3,
+    NONE = -1, /**< None encryption mode*/
+    LOW = 1, /**< Low encryption level. */
+    HIGH = 2, /**< High encryption level. */
+    EXTREME = 3, /**< Extreme encryption level. */
 } level_t;
 
+/**
+ * @brief Represents a character combination for encryption or decryption.
+ */
 typedef struct {
-    char input;
-    char output;
+    char input; /**< Represents a single character input to be processed. */
+    char output; /**< Represents a single character output resulting from processing. */
 } comb_t;
 
+/**
+ * @brief Represents a character combination for encryption or decryption in format int.
+ */
 typedef struct {
-    int input;
-    int output;
+    int input; /**< Represents a single character input to be processed. */
+    int output; /**< Represents a single character output resulting from processing. */
 } int_comb_t;
 
 #endif
